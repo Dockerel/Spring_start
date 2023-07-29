a46e1249cda8f44667f1be5c423ee9ac74411340
Dockerel <dgh0001@naver.com>
2023-07-29 21:50:11 +0900

    .

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -6,6 +6,16 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
+    // 필드 주입
+    // @Autowired private MemberService memberService;
+
+    // setter 주입 - public으로 설정되어 있어 중간에 바뀌면 문제 생김
+    // @Autowired
+    // public void setMemberService(MemberService memberService){
+    //     this.memberService = memberService;
+    // }
+
+    // 생성자 주입 - ✅ 요즘 권장하는 스타일. 생성 시점에 설정하고 변화 ❌
     private final MemberService memberService;
 
     @Autowired
