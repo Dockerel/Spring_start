738d21bc851d1831ac0ef37bdbaf5ad5556c4503
Dockerel <dgh0001@naver.com>
2023-07-29 21:03:37 +0900

    member service + test case added

---
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,22 @@
+package hello.hellospring.domain;
+
+public class Member {
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private Long id;
+    private String name;
+}
